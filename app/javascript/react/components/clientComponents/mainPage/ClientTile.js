b877f51f123ed50a5240a7db609df98216239142
Petar Tzonevski <Pete@Petars-MBP.fios-router.home>
2022-05-31 12:26:25 -0400

    latest

--- app/javascript/react/components/clientComponents/mainPage/ClientTile.js
+++ app/javascript/react/components/clientComponents/mainPage/ClientTile.js
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom"
 
 const ClientTile = props => {
-  const { id, salutation, first_name, middle_name, last_name, ssn, birth_date, comment } = props.client
+  const { id,
+          salutation,
+          first_name,
+          middle_name,
+          last_name, ssn,
+          birth_date,
+          comment } = props.client
 
   const handleDeleteClick = () => {
     props.deleteClient(id)
@@ -24,9 +30,9 @@ const ClientTile = props => {
       <td>{first_name}</td>
       <td>{middle_name && <>{middle_name}</>}</td>
       <td>{last_name}</td>
-      <td>{ssn && <>{ssn}</>}</td>
-      <td>{birth_date && <>{birth_date}</>}</td>
-      <td>{comment && <>{comment}</>}</td>
+      <td>{ssn}</td>
+      <td>{birth_date}</td>
+      <td>{comment}</td>
       <td><Link to={`/people/${id}`}>Show Client</Link></td>
       <td><button className="button-style" onClick={handleDeleteClick}>Delete</button></td>
       <td><button className="button-style" onClick={toggleEditMenu}>Edit</button></td>
